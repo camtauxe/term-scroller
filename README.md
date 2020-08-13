58ad883f6ac5ac6413c56b4a1863ede13fe1a440
Cameron Tauxe <camerontauxe@gmail.com>
2020-08-13 12:08:46 -0600

    module-starter

---
+++ README.md
@@ -0,0 +1,19 @@
+# Term-Scroller
+
+description
+
+## Installation
+
+```sh
+perl Build.PL
+./Build
+./Build test
+./Build install
+```
+
+# LICENSE AND COPYRIGHT
+
+This software is copyright (c) 2020 by Cameron Tauxe.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
---
+++ lib/Term/Scroller.pm
@@ -0,0 +1,34 @@
+package Term::Scroller;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.1';
+
+=head1 NAME
+
+Term::Scroller - [description]
+
+=head1 SYNOPSIS
+
+    use Term::Scroller;
+    # [usage here]
+
+=cut
+
+1;
+
+=head1 AUTHOR
+
+Cameron Tauxe C<camerontauxe@gmail.com>
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is copyright (c) 2020 by Cameron Tauxe.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
